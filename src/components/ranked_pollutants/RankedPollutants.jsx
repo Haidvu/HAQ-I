947cbf75c87c4e2b357f6afeccf8f67ef672610d
Hanh <57729860+Hanh-hub@users.noreply.github.com>
2021-12-05 20:28:59 -0600

    add rankedpollutants component

--- src/components/ranked_pollutants/RankedPollutants.jsx
+++ src/components/ranked_pollutants/RankedPollutants.jsx
@@ -1,118 +1,221 @@
-
-import React, { useState } from 'react';
-// import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
-
-import { makeStyles } from "@material-ui/core/styles";
-import { blue, orange} from '@material-ui/core/colors';
-// import { render } from 'react-dom'
-// import Highcharts from 'highcharts'
-// import HighchartsReact from 'highcharts-react-official'
-
-// import { TrendingUpRounded } from '@material-ui/icons';
-
-
-import { styled } from '@mui/material/styles';
+import React, { useState, useEffect, useRef } from "react";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs"
+import "./styles.css";
+import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import StackedBarChart from "./StackedBar"
-import HealthEffect from './HealthEffect';
-
-import axios from 'axios'
 
 
-function RankedPollutant(){
-  const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }))
+// messages to display to users for different AQI levels
+const messages = [
+    "Enjoy your outdoor activit",
+    "If you are unusually sensitive to particle pollution, consider reducing your activity level or shorten the amount of time you are active outdoors.",
+    "Unusually sensitive people should consider limiting prolonged outdoor exertion",
+    "Active children and adults and people with respiratory disease such as asthma  should avoid prolonged outdoor exertion. Everyone else, especially children,    should limit prolonged outdoor exertion",
+    "Active children and adults, and people with respiratory disease such as asthma,should avoid all outdoor exertion. Everyone else, especially children, should limit outdoor exertion.",
+    "Everyone should avoid all outdoor exertion."
+];
 
-console.log("render")  
-  var dataz
-  const [pollutants,setPollutants]=useState([])
-  const innitialPollutants=[0,0,0]
-  const innitialPara=['a','a','a']
-  let [aqi_data,setAQI]=useState(innitialPollutants)
-  let [para_name,setPara]=useState(innitialPara)
-  let [risk_rating,setRating]=useState(["null",'null',"null"])
-  const GetRankedPollutantsData = () => {
-    axios.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=055CDA36-3FC4-4E95-BBBE-73C265CF3131")
-    .then(res => {
-      console.log('airdata',res.data)
-      setPollutants(res.data);
+const getAqiLevelRatio = (aqiValue) => Math.round(aqiValue / 500 * 100, 2)
 
-      var AQIs=[res.data[2].AQI,res.data[1].AQI,res.data[0].AQI]
-      var names=[res.data[2].ParameterName,res.data[1].ParameterName,res.data[0].ParameterName]
-      var risks=[res.data[2].Category.Name,res.data[1].Category.Name,res.data[0].Category.Name]
-      setAQI(AQIs);
-      setPara(names);
-      setRating(risks);
-    })
-  }
-  
-  
+const getHealthConcernLevel = (aqiValue) => {
+    const levels = [
+        {
+            'title': "good",
+            "color": "lightgreen",
+            "message": messages[0]
+        },
+        {
+            'title': 'moderate',
+            'color': 'yellow',
+            "message": messages[1]
+        }, {
+            'title': 'unhealthy for sensitive groups',
+            'color': 'orange',
+            "message": messages[2]
+        }, {
+            'title': 'unhealthy',
+            'color': 'pink',
+            "message": messages[3]
+        }, {
+            'title': 'very unhealthy',
+            'color': 'purple',
+            "message": messages[4]
+        }, {
+            'title': 'hazardous',
+            'color': 'maroon',
+            "message": messages[5]
+        }]
 
-  
-  return(
-    
-      <div>
-     
-      <div>
-      
-        
-      <Box sx={{ flexGrow: 1 }}>
-       
-        <Grid container spacing={2}>
+    if (aqiValue >= 0 && aqiValue <= 50) {
+        return levels[0]
+    }
 
-          <Grid item xs={6}>
-            <Item>
-              {StackedBarChart(aqi_data,para_name)}
-             aqi: {aqi_data}
-             para: {para_name}
-             risk: {risk_rating}
-              {(typeof pollutants.data!="undefined")?(
-                <div>
-                  {GetRankedPollutantsData}
-                  aqi:{aqi_data}
-            {StackedBarChart()}
-                </div>):("")}          
-            </Item>
-          </Grid>
+    if (aqiValue >= 51 && aqiValue <= 100) {
+        return levels[1]
+    }
 
-          <Grid item xs={6}>
-            <Item>
-              {HealthEffect(para_name,aqi_data,risk_rating)}
-              {/* <HealthEffect></HealthEffect> */}
-            </Item>
-          </Grid>
-          
-        </Grid>
-        
-    </Box>
+    if (aqiValue >= 101 && aqiValue <= 150) {
+        return levels[2]
+    }
 
-      </div>
-      <button variant="contained" onClick={() => GetRankedPollutantsData()}>Refresh</button>
-      
-      <div>
-        {/* {
-          pollutants.length > 0 &&
-          pollutants.map(p => 
-            <>
-            <span>Latitude: {p.Latitude}<br/></span>
-            <span>AQI: {p.AQI}<br/></span>
-            </>
-          )
-          
-        }
-       */}
-      
-        
-      </div>
-    </div>
-  )
-  }
-export default RankedPollutant;
+    if (aqiValue >= 151 && aqiValue <= 200) {
+        return levels[3]
+    }
+
+    if (aqiValue >= 201 && aqiValue <= 300) {
+        return levels[4]
+    }
+
+    if (aqiValue >= 301) {
+        return levels[5]
+    }
+
+    return {
+        'title': 'undefined',
+        'color': 'gray',
+        'message': "Nothing"
+    }
+}
 
 
+const PollutantElement = ({ title, parameterName, rating, aqi, reportingArea, description }) => {
+    const [showHealthAdvice, setShowHealthAdvice] = useState(false);
+
+    return (
+        <div>
+            <div
+                style={{
+                    display: "flex",
+                    width: "320px",
+                    alignItems: "center",
+                    margin: "16px 0"
+                }}
+            >
+                <strong className="w-20">{parameterName ? parameterName : "No Data"}</strong>
+                <div className="w-20">
+                    <span className="aqi-circle" style={{
+                        backgroundColor: getHealthConcernLevel(Number(aqi)).color
+                    }}>
+                        {aqi ? aqi : "No AQI"}
+                    </span>
+                </div>
+
+                <span className="w-30">{rating}</span>
+                <span onClick={() => setShowHealthAdvice(!showHealthAdvice)}>
+                    {showHealthAdvice ? <BsChevronUp /> : <BsChevronDown />}
+                </span>
+            </div>
+            {showHealthAdvice && <div>{getHealthConcernLevel(Number(aqi)).message}</div>}
+        </div>
+    );
+};
+
+const ChartBar = ({ data }) => {
+    console.log(data)
+    return (
+        <div className="flex chartbar-item">
+            <span className="w-20 b">{data && data.ParameterName}</span>
+            <span className="w-10">{data && data.AQI}</span>
+            <div className="w-70">
+                <div className="chartbar-wrapper">
+                    <div className="chartbar-main" style={{
+                        width: `${data && getAqiLevelRatio(data.AQI)}%`
+                    }} />
+                    <div className="chartbar-bg">
+                        {
+                            [{
+                                width: 10,
+                                color: 'lightgreen'
+                            }, {
+                                width: 10,
+                                color: '#fbff17'
+                            }, {
+                                width: 10,
+                                color: '#ff7c17'
+                            }, {
+                                width: 10,
+                                color: 'red'
+                            }, {
+                                width: 20,
+                                color: 'purple'
+                            }, {
+                                width: 40,
+                                color: '#520909'
+                            }].map((item, index) =>
+                                <div key={index} className="chartbar-group" style={{
+                                    width: `${item.width}%`,
+                                    backgroundColor: item.color
+                                }} />
+                            )
+
+                        }
+                    </div>
+                </div>
+                {data && getAqiLevelRatio(data.AQI)}
+            </div>
+        </div>
+    )
+}
+
+const Charts = ({ data }) => {
+    return (
+        <div className="ph3">
+            <h2>Ranked by Pollutants</h2>
+            <div className="ph4">
+                {data && data.map((item, index) => <ChartBar data={item} key={index} />)}
+            </div>
+        </div>
+    )
+}
+
+const HealthEffects = ({ data }) => {
+    return (
+        <div>
+            <h2>Health Effect</h2>
+            {data && data.map((item, index) => <PollutantElement parameterName={item.ParameterName} aqi={item.AQI} rating={item.Category.Name} key={index} />)}
+        </div>
+    )
+}
+
+const RankedPollutant = () => {
+    const zipCodeInputEl = useRef(null)
+    const [isLoading, setLoading] = useState(true)
+    const [data, setData] = useState(null)
+    const [zipCode, setZipCode] = useState("77057")
+    let API_URL = `https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=${zipCode}&distance=25&API_KEY=055CDA36-3FC4-4E95-BBBE-73C265CF3131`;
+
+    useEffect(() => {
+        fetch(API_URL).then((res) => res.json()).then((data) => {
+            setData(data)
+            setLoading(false)
+        })
+    }, [zipCode])
+    return (
+        <Card>
+            <input
+                type="number"
+                ref={zipCodeInputEl}
+                defaultValue={zipCode}
+                onChange={(e) => setZipCode(e.target.value)}
+            />
+            <div className="flex">
+                <div className="w-70">
+                    {!data && isLoading && <span>Loading data</span>}
+                    {data && <Charts data={data} />}
+                    {!data && !isLoading && <span>Failed to load data</span>}
+                </div>
+                <div className="w-50">
+                    {!data && isLoading && <span>Loading data</span>}
+
+                    {data && <HealthEffects data={data} />}
+                    {!data && !isLoading && <span>Failed to load data</span>}
+                </div>
+            </div>
+
+
+
+        </Card>
+    )
+}
+
+export default RankedPollutant
