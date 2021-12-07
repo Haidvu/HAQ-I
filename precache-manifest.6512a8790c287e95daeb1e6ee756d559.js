d31959e137ca5dcfab02ced574b2a906e600dbbe
Hai Vu <55154371+Haidvu@users.noreply.github.com>
2021-12-06 20:02:12 -0600

    Updates

---
+++ precache-manifest.6512a8790c287e95daeb1e6ee756d559.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8956f16cd2629eceb9d862e8e4826cd9",
+    "url": "/HAQIAPP/index.html"
+  },
+  {
+    "revision": "54824143ae1424d82b75",
+    "url": "/HAQIAPP/static/css/2.bd66a91b.chunk.css"
+  },
+  {
+    "revision": "033b2321d26693932982",
+    "url": "/HAQIAPP/static/css/main.00950787.chunk.css"
+  },
+  {
+    "revision": "54824143ae1424d82b75",
+    "url": "/HAQIAPP/static/js/2.363d9a0f.chunk.js"
+  },
+  {
+    "revision": "71d4ec38a5ca7e2b8b5a4cd1cfca82a9",
+    "url": "/HAQIAPP/static/js/2.363d9a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9320d23c00cadb992054",
+    "url": "/HAQIAPP/static/js/3.e61918db.chunk.js"
+  },
+  {
+    "revision": "033b2321d26693932982",
+    "url": "/HAQIAPP/static/js/main.f5e4d7c4.chunk.js"
+  },
+  {
+    "revision": "4c5c5039ac7281204aac",
+    "url": "/HAQIAPP/static/js/runtime-main.325c882e.js"
+  },
+  {
+    "revision": "aa692d9ffc9a74828d6bcb880805ae0f",
+    "url": "/HAQIAPP/static/media/aqi_logo.aa692d9f.jpg"
+  }
+]);
